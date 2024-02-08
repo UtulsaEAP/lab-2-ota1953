1bc13bf88581750c6e99d7107f0bd805bd995a58
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-02-08 14:08:24 -0600

    Fourth Finish

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -6,14 +6,16 @@ def right_arrow():
     head = input()
 
     row1 = ('      ' + head)
-    row2 = (base + base + base + base + base + base + head)
+    row2 = (base + base + base + base + base + base + head + head)
     row3 = (base + base + base + base + base + base + head + head + head)
+    row4 = (base + base + base + base + base + base + head)
 
     print(row1)
     print(row2)
     print(row3)
     print(row2)
-    print(row1)
+    print(row4)
+
 
 if __name__ == "__main__":
     right_arrow()
